c3a1e514afc5e9b05c6b126e903eb0b3874a3757
JP <jp@jp.nl>
2025-03-24 13:47:18 +0100

    TDD laten zien

---
+++ demo/DemoSolution/DemoProject.MSTest/AutocompleterTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoProject.MSTest;
+
+[TestClass]
+public class AutocompleterTests
+{
+    Autocompleter<Car> _sut = null!;
+    List<Car> _data = null!;
+
+    [TestInitialize]
+    public void Init()
+    {
+        _data = new List<Car>
+        {
+            new() { Make = "Tesla", Model = "Model Y" },
+            new() { Make = "Peugeot", Model = "208" },
+            new() { Make = "Kia", Model = "Miro" },
+            new() { Make = "Opel", Model = "Astra" },
+            new() { Make = "Opel", Model = "Corsa" },
+            new() { Make = "Renault", Model = "Clio" },
+            new() { Make = "Volkswagen", Model = "Golf" },
+            new() { Make = "Renault", Model = "Twingo" },
+            new() { Make = "Fiat", Model = "Punto" },
+            new() { Make = "Toyota", Model = "Yaris" },
+        };
+        _sut = new Autocompleter<Car>();
+        _sut.Data = _data;
+    }
+
+
+    [TestMethod]
+    public void Autocomplete_HappyFlowQueryAndData_SuggestsMatchingCars()
+    {
+        // Arrange
+        _sut.Query = "s";
+
+        // Act
+        _sut.Autocomplete();
+
+        // Assert
+        Assert.AreEqual(5, _sut.Suggestions.Count);
+    }
+
+    [TestMethod]
+    public void Autocomplete_CapitalizedQuery_SuggestCaseInsensitively()
+    {
+        // Arrange
+        _sut.Query = "S";
+
+        // Act
+        _sut.Autocomplete();
+
+        // Assert
+        Assert.AreEqual(5, _sut.Suggestions.Count);
+    }
+
+    [TestMethod]
+    public void Autocomplete_QueryThatMatchesMultipleProperties_AddsSuggestionsUniquely()
+    {
+        // Arrange
+        _sut.Query = "i";
+
+        // Act
+        _sut.Autocomplete();
+
+        // Assert
+        Assert.AreEqual(5, _sut.Suggestions.Count);
+    }
+
+    [TestMethod]
+    public void Autocomplete_EmptyQuery_AllSuggestions()
+    {
+        // Arrange
+        _sut.Query = "";
+
+        // Act
+        _sut.Autocomplete();
+
+        // Assert
+        Assert.AreEqual(_data.Count, _sut.Suggestions.Count);
+    }
+
+    [TestMethod]
+    //[ExpectedException(typeof(NullReferenceException))]
+    public void Autocomplete_NoData_Throws()
+    {
+        // Arrange
+        _sut.Query = "e";
+        _sut.Data = null!;
+
+        // Act
+        //_sut.Autocomplete();
+        //var act = () => _sut.Autocomplete();
+
+        // Assert
+        Assert.ThrowsException<InvalidOperationException>(() => _sut.Autocomplete());
+    }
+}
---
+++ demo/DemoSolution/DemoProject/Autocompleter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject;
+
+public class Autocompleter<T>
+{
+    public string Query { get; set; }
+    public List<T> Data { get; set; }
+    public List<T> Suggestions { get; set; }
+
+    public void Autocomplete()
+    {
+        if (Data == null)
+        {
+            throw new InvalidOperationException("No data!");
+        }
+
+        Suggestions = new List<T>();
+
+        foreach (var item in Data)
+        {
+            var props = item.GetType().GetProperties();
+            foreach (var prop in props)
+            {
+                var value = prop.GetValue(item) as string;
+
+                if (value.Contains(Query, StringComparison.OrdinalIgnoreCase))
+                {
+                    Suggestions.Add(item);
+                    break;
+                }
+            }
+        }
+    }
+}
